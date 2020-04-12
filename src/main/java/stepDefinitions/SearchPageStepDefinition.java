11f060bb76a5f7e8a46eeba8d2ef653fd7c47b6b
Vicky274 <vimessi2@gmail.com>
2020-04-12 12:29:14 +0530

    addition method

--- src/main/java/stepDefinitions/SearchPageStepDefinition.java
+++ src/main/java/stepDefinitions/SearchPageStepDefinition.java
@@ -2,9 +2,10 @@ package stepDefinitions;
 
 public class SearchPageStepDefinition {
     //i have added a new file
-	
+	int b,c;
 	
 	public void add(){
 		//System.out.println("added some code");
+		int a=b+c;
 	}
 }
